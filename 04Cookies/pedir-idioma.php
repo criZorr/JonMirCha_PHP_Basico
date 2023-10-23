e81b3d00ded32c7b07e395a7ca1010d5028c03c5
criZorr <christophermjk@outlook.com>
2023-10-23 10:46:31 -0500

    05 - Cookies

---
+++ 04Cookies/pedir-idioma.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Cookies en PHP</title>
+    </head>
+    <body>
+        <a href="grabar-cookie.php?idioma=es">versión en español</a>
+        <br><br>
+        <a href="grabar-cookie.php?idioma=en">versión en inglés</a>
+    </body>
+</html>
