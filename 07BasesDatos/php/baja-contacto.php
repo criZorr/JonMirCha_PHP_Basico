1d53ff1aed4a3bb6acc5194b6c6ac9ba772fe686
criZorr <christophermjk@outlook.com>
2023-10-30 11:26:49 -0500

    13 - Aplicación CRUD (4/7)

--- 07BasesDatos/php/baja-contacto.php
+++ 07BasesDatos/php/baja-contacto.php
@@ -1,1 +1,18 @@
-BAJA
+<form id="baja-contacto" name="baja_frm" action="php/eliminar-contacto.php" method="post"
+    enctype="application/x-www-form-urlencoded">
+    <fieldset>
+        <legend>Baja de contacto</legend>
+        <div>
+            <label for="email">Email: </label>
+            <select name="email_slc" id="email" class="cambio" required>
+                <option value="">- - - - - - - - - - - -</option>
+                <?php include("select-email.php"); ?>
+            </select>
+        </div>
+        <div>
+            <input type="submit" id="enviar-baja" class="cambio" name="enviar-btn" value="eliminar">
+        </div>
+        <?php include("php/mensajes.php"); ?>
+    </fieldset>
+
+</form>
---
+++ 07BasesDatos/php/select-email.php
@@ -0,0 +1,9 @@
+<?php
+include("conexion.php");
+$consulta = "SELECT email FROM contactos ORDER BY email";
+$ejecutar_consulta = $conexion->query($consulta);
+while ($registro = $ejecutar_consulta->fetch_assoc()) {
+    echo "<option value='" . $registro["email"] . "'>" . $registro["email"] . "</option>";
+}
+$conexion->close();
+?>
